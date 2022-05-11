6b337efb25b783b51a36685b6c17309b713fb8dc
hugo3011mendez <hugo3011mendez@gmail.com>
2022-05-11 13:16:05 +0200

    MapObject : Añadido rango de valores, para que el usuario no ponga un valor de tamaño no deseado en el formulario
    Index :
    Modificado formulario para mostrar un mensaje de error, si el usuario escribe un valor no deseado del tamaño del mapa

--- Snake/Pages/MapObject.cs
+++ Snake/Pages/MapObject.cs
@@ -1,8 +1,12 @@
-﻿namespace Snake.Pages
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Snake.Pages
 {
     public class MapObject
     {
         // Hace referencia a las dimensiones en px del mapa, que será este número tanto para el eje X como el eje Y
+        [Range(100, 1000)] // Establezco un rango para el tamaño del mapa, para que no sea ni muy pequeño ni muy grande
         public int Size { get; set; }
 
         public MapObject(int size) // Constructor, pasándole el tamaño del mapa como parámetro
