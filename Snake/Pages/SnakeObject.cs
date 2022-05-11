b7b8090ba55ac002b3e6bae85e565a01120b91a5
Ismael Lage Groba <mrlage99@gmail.com>
2022-05-11 11:37:08 +0200

    Incluida la clase SnakeObject
    Creacion de la clase de serpiente para construir el juego

---
+++ Snake/Pages/SnakeObject.cs
@@ -0,0 +1,19 @@
+﻿namespace Snake.Pages
+{
+    public class SnakeObject
+    {
+
+
+        public int X { get; set; } 
+        public int Y { get; set; } 
+
+        public SnakeObject()
+        {
+           
+            X = 0;
+            Y = 0;
+        }
+
+
+    }
+}
