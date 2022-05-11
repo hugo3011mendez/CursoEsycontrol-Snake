83af26fef2dd28177164d546e1997a6367c31965
eusonoscar <oscaralvarezx2@gmail.com>
2022-05-11 13:30:03 +0200

    Update SnakeObject.cs

--- Snake/Pages/SnakeObject.cs
+++ Snake/Pages/SnakeObject.cs
@@ -1,12 +1,46 @@
-﻿namespace Snake.Pages
+﻿using Microsoft.AspNetCore.Components.Web;
+
+namespace Snake.Pages
 {
     public class SnakeObject : Props
     {
-
+        
         public SnakeObject()
         {
             X = 0;
             Y = 0;
+        }
+
+        public void moveUp(KeyboardEventArgs e)
+        {
+            if(e.Key == "vbKeyUp")
+            {
+                Y++;
+            }
+        }
+
+        public void moveDown(KeyboardEventArgs e)
+        {
+            if(e.Key == "vbKeyDown")
+            {
+                Y--;
+            }
+        }
+
+        public void moveRight(KeyboardEventArgs e)
+        {
+            if (e.Key == "vbKeyRight")
+            {
+                X++;
+            }
+        }
+
+        public void moveLeft(KeyboardEventArgs e)
+        {
+            if (e.Key == "vbKeyLeft")
+            {
+                X--;
+            }
         }
     }
 }
