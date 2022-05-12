af58fc7b4e2bea9d5c4b209454436574fed6e0b0
Ismael Lage Groba <mrlage99@gmail.com>
2022-05-12 13:32:24 +0200

    Revert "Añadido del array"
    This reverts commit aca8ce435820d81a5abaddd9d3d3a8536df0ddb2.

--- Snake/Pages/MapObject.cs
+++ Snake/Pages/MapObject.cs
@@ -10,24 +10,36 @@ namespace Snake.Pages
         [Range(100, 800)] // Establezco un rango para el tamaño del mapa, para que no sea ni muy pequeño ni muy grande
         public int Size { get; set; }
 
-       public  string[,] casillas { get; set; }
-
         /// <summary> Constructor pasándole su tamaño como parámetro </summary>
         /// <param name="size">Tamaño del mapa, en píxeles</param>
         public MapObject(int size)
         {
             Size = size;
-            string[,] casillas = new string[size, size];
         }
 
         /// <summary> Constructor vacío, por si el usuario no escribe ningún tamaño </summary>
         public MapObject()
         {
             Size = 400;
-            string[,] casillas = new string[Size, Size];
         }
 
-      
+        public void TableCreate()
+        {
+            string[,] names = new string[Size, Size];
+
+            for (int i = 0;i<names.Length;i++)
+            {
+
+                for (int z = 0; z < names.Length; z++)
+                {
+
+                    names[i,z] = "X";
+
+                }
+            }
+
+            
+        }
 
 
 
