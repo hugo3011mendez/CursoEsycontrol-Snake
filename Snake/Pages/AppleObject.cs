cf4ef420df0c42d628e17af09a68837ad82193fe
hugo3011mendez <hugo3011mendez@gmail.com>
2022-05-12 10:48:21 +0200

    AppleObject : Añadida propiedad color de tipo cadena, para especificar el color de la manzana, que se aplicará por CSS Modificado constructor pasando un color como parámetro y estableciendo su nueva variable

--- Snake/Pages/AppleObject.cs
+++ Snake/Pages/AppleObject.cs
@@ -2,10 +2,12 @@
 {
     public class AppleObject : Props // Este objeto representa a la manzana que la serpiente debe comer y así ganar más puntuación
     {
-        public AppleObject(int x, int y) // Constructor con su posición como parámetro
+        public string Color { get; set; } // Referente al color de la manzana, que lo aplicaremos via CSS
+        public AppleObject(int x, int y, string color) // Constructor con su posición como parámetro
         {
             PosX = x;
             PosY = y;
+            Color = color;
 
             // Se le establece un tamaño por defecto, las manzanas siempre serán del mismo tamaño
             SizeX = 5;
