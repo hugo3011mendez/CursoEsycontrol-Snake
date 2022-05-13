c13b48ca6a5d4a0c42b9e5dd360ece747abab01b
Ismael Lage Groba <mrlage99@gmail.com>
2022-05-13 11:03:34 +0200

    Update AppleObject.cs
    sdads

--- Snake/Pages/AppleObject.cs
+++ Snake/Pages/AppleObject.cs
@@ -7,7 +7,7 @@ namespace Snake.Pages
     public class AppleObject : Props, IDisposable 
     {
         /// <summary>Referente al color de la manzana, que lo aplicaremos via CSS</summary>
-        public string Color { get; set; }
+        
         
 
         /// <summary>Constructor con su posición y su color como parámetros</summary>
@@ -15,7 +15,7 @@ namespace Snake.Pages
         {
             PosX = x;
             PosY = y;
-            Color = "red";
+            
 
             // Se le establece un tamaño por defecto, las manzanas siempre serán del mismo tamaño
             SizeX = 5;
