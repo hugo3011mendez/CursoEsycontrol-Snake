461e7df6654787abde821fbf48830ad4db450360
hugo3011mendez <hugo3011mendez@gmail.com>
2022-05-13 11:34:08 +0200

    SnakeObject : Quitado el parámetro nivel del constructor, y añadidos todos los comentarios pertinentes Modificado tamaño por defecto Quitada propiedad Velocidad
    Index :
    Modificada creación de la instancia de la serpiente, acorde a los cambios en su constructor
    
    AppleObject :
    Realizados comentarios correctamente

--- Snake/Pages/AppleObject.cs
+++ Snake/Pages/AppleObject.cs
@@ -6,37 +6,23 @@ namespace Snake.Pages
     /// Hereda de Props e implementa la interfaz IDisposable</summary>
     public class AppleObject : Props, IDisposable 
     {
-        /// <summary>Referente al color de la manzana, que lo aplicaremos via CSS</summary>
-        
-        
-
-        /// <summary>Constructor con su posición y su color como parámetros</summary>
+        /// <summary> Constructor con sus parámetros representando su posición </summary>
         public AppleObject(int x, int y)
         {
             PosX = x;
             PosY = y;
-            
 
-            // Se le establece un tamaño por defecto, las manzanas siempre serán del mismo tamaño
-            SizeX = 5;
-            SizeY = 5;
+            // Tamaño por defecto de 1 casilla
+            SizeX = 1;
+            SizeY = 1;
         }
 
         public void CrearManzana(int mapax,int mapay){
-
-
             this.Dispose();
 
             AppleObject manzana = new AppleObject(mapax,mapay);
-
-        
         }
 
-        public void Dispose()
-        {
-
-
-        }
-
+        public void Dispose(){}
     }
 }
